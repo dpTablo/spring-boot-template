3b4ab537ef790b4afce5af5eb7552c0fb1b62800
dpTablo <leeyw2355@gmail.com>
2022-10-03 23:15:48 +0900

    테스트 케이스의 @Qulifier 설정 추가

--- src/test/java/com/dptablo/template/springboot/controller/UserApiControllerTest.java
+++ src/test/java/com/dptablo/template/springboot/controller/UserApiControllerTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -52,6 +53,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserApiControllerTest {
     private MockMvc mockMvc;
 
+    @Qualifier("defaultUserService")
     @MockBean
     private UserService userService;
 
